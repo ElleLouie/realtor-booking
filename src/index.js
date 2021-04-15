e230ab886a410ef3e2a729767e503baa968c4504
ElleLouie <ellelouie13@gmail.com>
2021-04-15 17:04:18 -0400

    5pm SA2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-const $ = require('jquery');
+import $ from 'jquery';
+import './style.css';
 
-$('#main').html('Here we go!');
+let sec = 0;
+
+setInterval(() => {
+  sec += 1;
+  $('#main').html(`You have been on this page for ${sec} seconds.`);
+}, 1000);
