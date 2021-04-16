f3242c1217c3aab49922d6933a5fe9c31721e43c
ElleLouie <ellelouie13@gmail.com>
2021-04-16 12:58:26 -0400

    SA3 starter index edit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import $ from 'jquery';
-import './style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';
 
 const App = () => <div className="test">All the REACT are belong to us!</div>;
-
 ReactDOM.render(<App />, document.getElementById('main'));
-
-let sec = 0;
-
-setInterval(() => {
-  sec += 1;
-  $('#main').html(`You have been on this page for ${sec} seconds.`);
-}, 1000);
