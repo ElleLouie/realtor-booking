8269daef3c15851b3bf3ae84a5b0376ac245220b
ElleLouie <ellelouie13@gmail.com>
2021-04-16 18:32:55 -0400

    SA2 index.js edit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import $ from 'jquery';
-import './style.scss';
-=======
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
->>>>>>> f3242c1217c3aab49922d6933a5fe9c31721e43c
+import './style.scss';
 
 const App = () => <div className="test">All the REACT are belong to us!</div>;
 ReactDOM.render(<App />, document.getElementById('main'));
